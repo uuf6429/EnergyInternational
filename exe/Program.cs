3a9c3bf6f4e445c288a8685ff8ff306c3a72564b
Christian Sciberras <christian@sciberras.me>
2014-04-11 12:55:49 +0200

    Update Program.cs

--- exe/Program.cs
+++ exe/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
 
 namespace EnergyInternational
 {
@@ -12,11 +14,37 @@ namespace EnergyInternational
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            if (args.Length == 2)
+            {
+                string CurrentExe = Process.GetCurrentProcess().MainModule.FileName;
+                switch (args[0])
+                {
+                    case "replace":
+                        Console.WriteLine("Replacing old version...");
+                        File.Copy(CurrentExe, args[1], true);
+                        Console.WriteLine("Cleaning up...");
+                        Process.Start(args[1], "delete \"" + CurrentExe + "\"");
+                        break;
+                    case "delete":
+                        int c = 0, m=10;
+                        while (File.Exists(args[1]) && c < m)
+                        {
+                            Console.WriteLine("Deleting old version (" + (++c) + "/" + m + ")...");
+                            File.Delete(args[1]);
+                        }
+                        Console.WriteLine("Running new version...");
+                        Process.Start(CurrentExe);
+                        break;
+                }
+            }
+            else
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
         }
     }
 }
